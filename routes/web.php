23105a0509f7b5ef613a10c0e5e2858a0fa4df07
Lexa <alecseicolomiets@gmail.com>
2026-06-04 23:26:58 +0500

    chore: finalize and optimize

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'name'    => 'QAZ DRIVE API',
+        'version' => '1.0.0',
+        'status'  => 'running',
+        'docs'    => 'http://127.0.0.1:8000/api',
+    ]);
 });
